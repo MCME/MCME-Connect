5e138ceedec90aa306ac5874568df8b3c07e759d
Nicky Vermeersch <afotai@gmail.com>
2026-07-26 00:20:26 +0200

    build: add JUnit 5 test harness

---
+++ src/test/java/com/mcmiddleearth/connect/HarnessTest.java
@@ -0,0 +1,13 @@
+package com.mcmiddleearth.connect;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class HarnessTest {
+
+    @Test
+    void testHarnessRuns() {
+        assertTrue(true);
+    }
+}
